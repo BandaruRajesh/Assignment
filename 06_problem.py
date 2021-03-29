0c07a700706a31c6e6a312462c8d052cf79d961d
SrinivasaRaoGadu <70016657+SrinivasaRaoGadu@users.noreply.github.com>
2021-03-29 16:03:30 +0530

    Update 06_problem.py

--- 06_problem.py
+++ 06_problem.py
@@ -1,33 +1,42 @@
 """
-??? Question with two examples ???
+Given a lowercase alphabet string s, return the total number of substrings that contain one unique character.
+
+Constraints
+
+n ≤ 10,000 where n is the length of s.
+Example 1
+Input
+s = "aab"
+Output
+4
+Explanation
+These substrings have one unique character: ["a", "a", "aa", "b"]
+
 """
+
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def solve(s):
     pass
 
-
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestIsPrime(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(solve("dgoiowrbsnbrfnrpwnoptemenodlnbgfbvilbiuaeribuebeoatb"), 52)
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(solve("egwprhwobobadvidbgepqbpdnbpnhornibadpbphrposiveqiobpqbcsavfoveuvgoe"), 67)
+
+    def test_03(self):
+        self.assertEqual(solve("sionpwobpwoeppghaucxyatsdyvbjsvlfkhpsrojdlvsdvugoafsalcaksfa"), 61)
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
