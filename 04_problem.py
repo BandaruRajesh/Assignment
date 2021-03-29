49aad2a78e9baa5bee04090cbeee9c45d17101e8
SrinivasaRaoGadu <70016657+SrinivasaRaoGadu@users.noreply.github.com>
2021-03-29 16:03:07 +0530

    Update 04_problem.py

--- 04_problem.py
+++ 04_problem.py
@@ -1,32 +1,36 @@
-"""
-??? Question with two examples ???
-"""
+'''Given a list of integers nums sorted in ascending order and an integer k, return whether any two elements from the list add up to k. You may not use the same element twice.
+Constraints
+n ≤ 100,000 where n is the length of nums
+Example 1
+Input
+nums = [1, 3, 5, 8]
+k = 6
+Output
+True
+Explanation
+We can have 1 + 5 = 6.
+'''
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def solve(nums,k):
     pass
 
-
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestReverseSentence(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(solve([1, 3, 5, 8],7), False)
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(solve([1,6,24,55,89,234,1245],1246), True)
+
+    def test_03(self):
+        self.assertEqual(solve([12,34,56,66,6666,34982,1213414],66), False)
 
 
 if __name__ == '__main__':
