d7e81c3ebd07484c961f4590f13f2cdef259cefd
SrinivasaRaoGadu <70016657+SrinivasaRaoGadu@users.noreply.github.com>
2021-03-29 16:04:33 +0530

    Update 10_problem.py

--- 10_problem.py
+++ 10_problem.py
@@ -1,33 +1,36 @@
 """
-??? Question with two examples ???
+Given an integer n, return the maximum number you can make by inserting 5 anywhere in the number.
+
+Example 1
+Input
+n = 923
+Output
+9523
 """
+
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def solve(n):
     pass
 
-
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestIsPrime(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(solve(2414135), 52414135)
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(solve(13123), 513123)
+
+    def test_03(self):
+        self.assertEqual(solve(-999999), -5999999)
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
