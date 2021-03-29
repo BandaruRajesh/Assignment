5564432a6a5fe95253858b7120c48cf6d2f3cd9f
SrinivasaRaoGadu <70016657+SrinivasaRaoGadu@users.noreply.github.com>
2021-03-29 16:02:04 +0530

    Update 02_problem.py

--- 02_problem.py
+++ 02_problem.py
@@ -1,32 +1,41 @@
 """
-??? Question with two examples ???
+You are given a string s containing digits from "0" to "9" and lowercase alphabet characters. Return the sum of the numbers found in s.
+
+Constraints
+
+1 ≤ n ≤ 100,000 where n is the length of s
+Example 1
+Input
+s = "11aa32bbb5"
+Output
+48
+Explanation
+Since 11 + 32 + 5 = 48.
+
 """
+
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def solve(s):
     pass
 
 
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestIsPrime(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(solve("1a2b30"), 33)
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(solve("abc"), 0)
+
+    def test_03(self):
+        self.assertEqual(solve("11ibiub13etpiwhigw878sdwrws87hs8h"), 997)
 
 
 if __name__ == '__main__':
