dfdae1e7618ea6e0bfb440a376ceaa33f3b0a1d0
SrinivasaRaoGadu <70016657+SrinivasaRaoGadu@users.noreply.github.com>
2021-03-29 16:01:42 +0530

    Update 01_problem.py

--- 01_problem.py
+++ 01_problem.py
@@ -1,33 +1,41 @@
 """
-??? Question with two examples ???
-"""
+Given a positive integer n, determine whether you can make n by summing up some non-negative multiple of 3 and some non-negative multiple of 7.
+
+Constraints
+
+n < 2 ** 31
+Example 1
+Input
+n = 13
+Output
+True
+Explanation
+We can get 13 with 1 * 7 + 2 * 3."""
+
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def solve(n):
     pass
 
 
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestIsPrime(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(solve(24),True)
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(solve(95746), True)
+
+    def test_03(self):
+        self.assertEqual(solve(1), False)
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
