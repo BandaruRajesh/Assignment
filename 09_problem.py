b5fbce8f8990be46e833dd58f6bfea582d5a84cd
SrinivasaRaoGadu <70016657+SrinivasaRaoGadu@users.noreply.github.com>
2021-03-29 16:04:21 +0530

    Update 09_problem.py

--- 09_problem.py
+++ 09_problem.py
@@ -1,33 +1,47 @@
 """
-??? Question with two examples ???
+The Fibonacci sequence goes like this: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+The next number can be found by adding up the two numbers before it, and the first two numbers are always 1.
+
+Write a function that takes an integer n and returns the nth Fibonacci number in the sequence.
+
+Constraints
+n ≤ 30
+
+Example 1
+Input
+n = 1
+Output
+1
+Explanation
+This is the base case and the first fibonacci number is defined as 1.
 """
+
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def solve(n):
     pass
 
-
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestIsPrime(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(solve(24), 46368)
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(solve(12), 144)
+
+    def test_03(self):
+        self.assertEqual(solve(30), 832040)
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
+
+
