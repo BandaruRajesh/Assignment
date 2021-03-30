3845564ec2383d09a6abf8ab7e1fcd09db7ab8c8
Taraka Rama Rao <tgottapu@gitam.edu>
2021-03-30 15:58:37 +0530

    First Update

--- 24_problem.py
+++ 24_problem.py
@@ -1,32 +1,41 @@
-"""
-??? Question with two examples ???
-"""
+'''
+Largest Gap
 
-import unittest
+Given a list of integers n,
+return the largest difference of two consecutive integers in the sorted version of n.
 
+Example 1:
+Input:
+n = [4, 1, 2, 8, 9, 10]
+Output:
+4
+Explanation:
+The largest gap is between 4 and 8 is 4
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+Example 2:
+Input:
+n = [5, 2, 3, 1, 10, 11]
+Output:
+5
+Explanation:
+The largest gap is between 5 and 10 is 5
+'''
+
+#Implement the below function and run this file
+# Return the output, No need to read input or print the output
+
+def largestGap(n):
     pass
 
+#DO NOT TOUCH THE BELOW CODE
 
-# Add these test cases, and remove this placeholder
+class TestlargestGap(unittest.TestCase):
 
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
+    def test_1(self):
+        self.assertEqual(largestGap([4, 1, 2, 8, 9, 10]), 4)
 
-# DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
-
-    def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
-
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_2(self):
+        self.assertEqual(largestGap([5, 2, 3, 1, 10, 11]), 5)
 
 
 if __name__ == '__main__':
