8559cf7f1f50cdb69a2ef653b86bdb9613750290
Taraka Rama Rao <tgottapu@gitam.edu>
2021-03-30 15:50:08 +0530

    First Update

--- 20_problem.py
+++ 20_problem.py
@@ -1,32 +1,44 @@
-"""
-??? Question with two examples ???
-"""
+'''
+Longest Substring with 2 Distinct Characters
 
-import unittest
+Given a string s, find the length of the longest substring that contains at most 2 distinct characters.
 
+Example 1:
+Input:
+s = "abccb"
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+Output:
+4
+
+Explanation:
+"bccb" is the longest substring with at most 2 distinct characters.
+
+Example 2:
+Input:
+s = "abddddbcdccb"
+
+Output:
+6
+
+Explanation:
+"bddddb" is the longest substring with at most 2 distinct characters.
+'''
+
+#Implement the below function and run this file
+# Return the output, No need to read input or print the output
+
+def longestSub(s):
     pass
 
+#DO NOT TOUCH THE BELOW CODE
 
-# Add these test cases, and remove this placeholder
+class TestlongestSub(unittest.TestCase):
 
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
+    def test_1(self):
+        self.assertEqual(longestSub("abccb"), 4)
 
-# DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
-
-    def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
-
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_2(self):
+        self.assertEqual(longestSub("abddddbcdccb"), 6)
 
 
 if __name__ == '__main__':
