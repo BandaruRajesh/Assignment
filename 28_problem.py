aa781f74e572f8f88c6f7e3afb8b43fc1303e26c
VSVS Murthy <murthy522@gmail.com>
2021-03-31 10:05:11 +0530

    assignment

--- 28_problem.py
+++ 28_problem.py
@@ -1,33 +1,37 @@
-"""
-??? Question with two examples ???
-"""
+'''Given a string s of words delimited by spaces, reverse the order of words.
+
+Constraints
+
+n ≤ 100,000 where n is the length of s
+Example 1
+Input
+sentence = "hello there my friend"
+Output
+"friend my there hello" '''
 
 import unittest
 
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
 
-def method_name(nums):
-    """
-    ??? Write what needs to be done ???
-    """
+
+def reverse_sentence(n):
     pass
 
 
-# Add these test cases, and remove this placeholder
-
-# 1. Test Cases from the Examples of Problem Statement
-# 2. Other Simple Cases
-# 3. Corner/Edge Cases
-# 4. Large Inputs
-
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestReverseSentence(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        self.assertEqual(reverse_sentence("hello there my friend"), "friend my there hello")
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        self.assertEqual(reverse_sentence("hello world"), "world hello")
+
+    def test_03(self):
+        self.assertEqual(reverse_sentence("welcome to class"), "class to welcome")
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
