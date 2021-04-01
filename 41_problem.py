56a8fa7f86ee17920a545007f1c05eb1e9bbd60b
BandaruRajesh <rbandaru3@gitam.edu>
2021-04-01 12:31:10 +0530

    First update

--- 41_problem.py
+++ 41_problem.py
@@ -1,11 +1,23 @@
 """
-??? Question with two examples ???
+Write a Program to print only the numbers which starts with digit which is prime and end with digit which is divisible by 5 upto a given integer n
+
+Example 1  
+Input
+n=5
+Output
+[5,20,25,30,35]
+
+Example 2  
+Input
+n=10
+Output
+[5,20,25,30,35,50,55,70,75,200]
 """
 
 import unittest
 
 
-def method_name(nums):
+def PrimeDiv(nums):
     """
     ??? Write what needs to be done ???
     """
@@ -20,13 +32,17 @@ def method_name(nums):
 # 4. Large Inputs
 
 # DO NOT TOUCH THE BELOW CODE
-class TestMethodName(unittest.TestCase):
+class TestPrimeDiv(unittest.TestCase):
 
     def test_01(self):
-        input_nums = [1, 2, 3, 4]
-        output_nums = 10
+        
+        self.assertEqual(PrimeDiv(5),[5,20,25,30,35])
 
-        self.assertEqual(method_name(input_nums), output_nums)
+    def test_02(self):
+        
+        self.assertEqual(PrimeDiv(10),[5,20,25,30,35,50,55,70,75,200])
+
+    
 
 
 if __name__ == '__main__':
